1faaa759c2b561dd357cf11e9f0f781d9eeef1ff
Daniel <caste0322@gmail.com>
2022-09-06 19:12:35 -0500

    Se soluciona error en las etiquetas a

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -7,14 +7,14 @@ function ProjectCard({ title, description, picture, code, url }) {
     <div className="project">
       <span className="project__title"> {title} </span>
       <div className="project__img">
-        <img src={picture} alt="project image" />
+        <img src={picture} alt="project screenshot" />
       </div>
       <p className="project__description"> {description} </p>
       <div className="project__cta">
-        <a target="_blank" className="project__code" href={code}>
+        <a rel="noopener" target="_blank" className="project__code" href={code}>
           <VscCode></VscCode>
         </a>
-        <a target="_blank" className="project__view" href={url}>
+        <a rel="noopener" target="_blank" className="project__view" href={url}>
           <MdRemoveRedEye></MdRemoveRedEye>
         </a>
       </div>
